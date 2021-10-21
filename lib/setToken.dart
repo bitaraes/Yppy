e5fdc890863832890ec9543b606034be536ff0db
Igor <igor_oliveira02@hotmail.com>
2021-10-21 00:47:54 -0300

    add http lib

--- lib/setToken.dart
+++ lib/setToken.dart
@@ -1,7 +1,6 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:dio/dio.dart';
-
-var dio = Dio();
+import 'package:http/http.dart' as http;
 
 setToken(token) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -40,19 +39,17 @@ getPosts() async {
   var token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwYjdmZTk5NzU1MzExMzI5MDU0NjhhOCIsImlhdCI6MTYzNDI0NDI4NywiZXhwIjoxNjM0ODQ5MDg3fQ.Y4e9VtQcQB-PB86xieek0I8Fk_n4LxZd6H1-bRcMOJo";
   try {
-    final response = await dio.get<List>('http://10.0.2.2:8080/comics',
-        options: Options(
-            headers: {"Authorization": 'Bearer $token'},
-            followRedirects: false,
-            validateStatus: (status) {
-              return status <= 500;
-            }));
+    var response = await http.get(
+      Uri.http('10.0.2.2:8080', "/comics"),
+      headers: {"Authorization": 'Bearer $token'},
+    );
+    var json = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      return response.data;
+      return json;
     } else {
       print('Ocorreu um erro');
     }
-  } on DioError catch (e) {
-    throw Exception(e);
+  } catch (e) {
+    print(e);
   }
 }
